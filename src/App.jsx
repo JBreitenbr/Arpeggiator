8346f8a77657109a57856f78b5936750ab8099d5
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-06-23 08:08:52 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
-
+import * as Tone from 'tone';
 export default function App() {
+  const synth=new Tone.Synth().toDestination();
+  const playSound=()=>{
+    synth.triggerAttackRelease("C4","8n");
+  }
   return (
+    <>
+    <header>
+      <ul>
+        <li><label htmlFor="chord-1">1</label></li>
+        <li><label htmlFor="chord-2">2</label></li>
+        <li><label htmlFor="chord-3">3</label></li>
+        <li><label htmlFor="chord-4">4</label></li>
+        <li><label htmlFor="chord-5">5</label></li>
+      </ul>
+    </header>
     <main>
-      React ⚛️ + Vite ⚡ + Replit
-    </main>
+      <button onClick={playSound}>Play Sound </button>
+    </main></>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sass from 'sass';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/Arpeggiator/',
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass,
+      },
+    },
+  },
   server: {
     host: '0.0.0.0',
+    allowedHosts: true
   }
 })
